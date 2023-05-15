0b0e9b3ab777a64789b68bcc90fc4043f026b1cc
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-05-15 04:11:18 +0100

    enable cors and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    credentials: true,
+    origin: function (origin, callback) {
+      console.log(`Origin ${origin} is being granted CORS access`);
+      callback(null, true);
+    },
+  });
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
