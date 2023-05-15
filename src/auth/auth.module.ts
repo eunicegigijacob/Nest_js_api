2b2b26f4b71f68873f64380eddd11911b3fedda0
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-05-15 04:13:47 +0100

    add importa for auth module and configure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/users.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+// import { jwtConstants } from './jwt.constants';
+import { config } from 'dotenv';
+config();
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRETE,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UsersService],
+})
 export class AuthModule {}
