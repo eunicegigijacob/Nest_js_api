095aa45e6c03e35d7d0f8f2d8226b9fadead1e3d
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-05-15 04:23:39 +0100

    add file imports for user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
