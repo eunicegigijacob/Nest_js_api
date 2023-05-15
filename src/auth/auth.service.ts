ea9127ad4624006c46d0384b1f2c441f7b63f45c
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-05-15 04:14:09 +0100

    add code for authentication services

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDTO } from './dto/signup.dto';
+import { LoginDTO } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(UsersService)
+    private readonly usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(signUpDTO: SignUpDTO): Promise<object> {
+    let status: object = {
+      success: true,
+      message: 'SignUp sucessful',
+    };
+
+    try {
+      await this.usersService.createUser(signUpDTO);
+    } catch (error) {
+      status = {
+        success: false,
+        message: error,
+      };
+    }
+    return status;
+  }
+
+  async login(loginDTO: LoginDTO): Promise<any> {
+    const payload = await this.usersService.userLogin(loginDTO);
+
+    return this.jwtService.sign(payload);
+  }
+}
